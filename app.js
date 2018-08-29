3a7c8651e52640748b367203924ecc8e6a944eb8
staffordc <stafford.carh@gmail.com>
2018-08-29 16:38:51 -0400

    Maybe have concats working

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ function loadDataAjax(){
             });
         }
     };
-    xhttp.open("Get", "https://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086&FcstType=json", true);
+    ///url reference substring split
+    var RefUrl = "https://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086&FcstType=json"
+    BaseLat = RefUrl.split("https://forecast.weather.gov/MapClick.php?lat=")
+    BaseLon = RefUrl.split("https://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=")
+    BaseEnd = RefUrl.split("https://forecast.weather.gov/MapClick.php?lat=38.4247341&lon=-86.9624086")
+
+    ///input boxes 1info and 2info 
+    FinUrl = BaseLat.concat(input1, BaseLon, input2, BaseEnd)
+
+    ///url reference (box1, on split 1) (box2, on split 2)
+    xhttp.open("Get", FinUrl, true);
+
     xhttp.send();
 }
